476644dd9dc69d348ff4598a3d9bb41b691eb16f
Sébastien Hugot <76730884+teachingbeforedying@users.noreply.github.com>
2025-04-24 16:57:29 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -19,35 +19,115 @@ app.use(express.static('public'));
 app.use(cors());
 
 
+function templateNameForRouteName(simpleRouteName) {
+    var outString;
 
+    switch(simpleRouteName) {
+        case "":
+            outString = "home";
+            break;
 
-/**
- * Return homepage
- * @name GET /login
- * @function
- */
-app.get('/', (req, res) => {
-//  res.sendFile('index.html', { root: __dirname }); //root = start on actual folder
-    
-    //console.log("app.get", "req:", req);
-    //console.log("app.get", "res:", res);
+        case "home":
+        case "about":
+        case "tutorial":
+        case "donate2":
+            outString = simpleRouteName;
+            break;
 
-    // res.sendFile('a-user-interface-main/3DRoom+ic.html', { root: __dirname }); //root = start on actual folder
+        case "create":
+        default:
+            outString = null;
+            break;
+    }
 
-    fs.readFile(__dirname + "/a-user-interface-main/3DRoom+ic.html", 'utf8', function (error, html) {
-        if (error) {
-            throw error;
-        }
+    return outString;
+}
+
+function app_get__template(simpleRouteName) {
+
+    function func__prepareMeal(simpleRouteName, req, res) {
+
+        //console.log("app.get", "req:", req);
+        //console.log("app.get", "res:", res);
+        // console.log("func__prepareMeal", "simpleRouteName:", simpleRouteName);
+
+        const templateName = templateNameForRouteName(simpleRouteName);
+
+        if(templateName != null) {
+
+            fs.readFile(__dirname + "/a-user-interface-main/3DRoom+ic.html", 'utf8', function (error, html) {
+                if (error) {
+                    throw error;
+                }
+                
+                // console.log("app.get", "html:", html);
+                const filepath__infinishute = `saves/${templateName}.shu`;
+                const html__new = AUI_Injector.injectLoadInfinishute(html, filepath__infinishute);
         
-        console.log("app.get", "html:", html);
+                res.end(html__new);
+            });
 
-        const filepath__infinishute = "saves/home.shu";
-        const html__new = AUI_Injector.injectLoadInfinishute(html, filepath__infinishute);
+        } else {
+            res.sendFile('a-user-interface-main/3DRoom+ic.html', { root: __dirname }); //root = start on actual folder
+        }
 
-        res.end(html__new);
+    }
+
+    app.get(`/${simpleRouteName}`, (req, res) => {
+        func__prepareMeal(simpleRouteName, req, res);
     });
 
+    app.get(`/${simpleRouteName}/`, (req, res) => {
+        func__prepareMeal(simpleRouteName, req, res);
+    });
+
+}
+
+
+
+
+
+// /**
+//  * Return homepage
+//  * @name GET /login
+//  * @function
+//  */
+// app.get('/', (req, res) => {
+// //  res.sendFile('index.html', { root: __dirname }); //root = start on actual folder
+    
+//     //console.log("app.get", "req:", req);
+//     //console.log("app.get", "res:", res);
+
+//     // res.sendFile('a-user-interface-main/3DRoom+ic.html', { root: __dirname }); //root = start on actual folder
+
+//     fs.readFile(__dirname + "/a-user-interface-main/3DRoom+ic.html", 'utf8', function (error, html) {
+//         if (error) {
+//             throw error;
+//         }
+        
+//         console.log("app.get", "html:", html);
+
+//         const filepath__infinishute = "saves/home.shu";
+//         const html__new = AUI_Injector.injectLoadInfinishute(html, filepath__infinishute);
+
+//         res.end(html__new);
+//     });
+
+// });
+
+app.get('/', (req, res) => {
+    res.redirect('/home');
 });
+
+app_get__template("home");
+
+app_get__template("tutorial");
+app_get__template("create");
+app_get__template("about");
+
+app_get__template("donate2");
+
+
 
 const auiPath = path.join(__dirname, 'a-user-interface-main');
 app.use(express.static(auiPath));
@@ -59,14 +139,6 @@ const savesPath = path.join(__dirname, 'a-user-interface-main/saves');
 app.use(express.static(savesPath));
 
 
-/**
- * Return the form page
- * @name GET /form
- * @function
- */
-app.get('/form', (req, res) => {
- res.sendFile('public/templates/form.html', { root: __dirname });
-});
 
 
 /**
